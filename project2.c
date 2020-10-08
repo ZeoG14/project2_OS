4d673bf4ed2fac73907ee2c21963416613770fd4
Eliseo Garcia <zeog14@gmail.com>
2020-10-07 19:30:19 -0600

    added token code

--- project2.c
+++ project2.c
@@ -14,12 +14,32 @@ int main (int argc, char **argv)
 	// Opening files "grades.bin" if it does not exist we are creating it. Only user can read and write
 	int fd = open("grades.bin", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 
-	if(fd == -1 ){
+	if(fd == -1 )
+	{
 		perror("Unable to open file");
 		exit(-1);
 	};
 
+	
+	while(fgets(inBuffer,BUFFERSIZE, stdin))
+	{
+		tokens[0]  = strtok(inBuffer,SEPERATORS);
+		if(tokens[0] == NULL)
+		{
+				fprintf(stderr, "No Arguments\n");
+		}
+		else
+		{
+				int size = 1;
+				for(int i = 0;tokens[i];++1){
+						tokens[i+1] = strtok(NULL, SEPERATORS);
+						size++;
+				}
+		}
 
+	}
+
+	 char command = tokens[0][0];
 
 	
 	return (0);

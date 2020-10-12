9d8edcf75e95d56fd51afd4f377b0c2e5194e538
Eliseo Garcia <zeog14@gmail.com>
2020-10-11 20:51:35 -0600

    assinging all values to the Record

--- project2.c
+++ project2.c
@@ -1,16 +1,17 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <string.h>
 # include "record.h"
 
-const int BUFFERSIZE = 112;
-const char SEPERATORS[] = " \t\n";
-const int MAXSIZE = 20;
 
-void append(char** );
+const int MAXSIZE = 20;
+const int SIZE_TYPE_NAMES = 4;
+
+void append(char**, int );
 void score(char**);
 
 
@@ -20,17 +21,15 @@ void score(char**);
 
 int main (int argc, char** argv)
 {	
-     char inBuffer[BUFFERSIZE];
-	 char* tokens[MAXSIZE];
-     // Opening files "grades.bin" if it does not exist we are creating it. Only user can read and write
-	 int fd = open("grades.bin", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+		// Opening files "grades.bin" if it does not exist we are creating it. Only user can read and write
+		 int fd = open("grades.bin", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 
-	// Writing error to stderr if file could not be opened
-	if(fd == -1 )
-	{
-		perror("Unable to open file");
-		exit(-1);
-	};
+		// Writing error to stderr if file could not be opened
+		if(fd == -1 )
+		{
+				perror("Unable to open file");
+				exit(-1);
+		};
 
 		char* command = argv[1];
 
@@ -45,7 +44,7 @@ int main (int argc, char** argv)
 		}
 
 		if(strcmp("append",command) == 0 ){
-				append(argv);
+				append(argv,fd);
 		}
 
 
@@ -60,12 +59,20 @@ int main (int argc, char** argv)
 		   */
 
 
+
+
+
+		if(close(fd) != 0){
+				perror("Close error");
+				exit(-1);
+		}
+
 	return (0);
 }
 
 
 
-void append(char** args){
+void append(char** args, int fd){
 
 		/*
 
@@ -76,7 +83,7 @@ void append(char** args){
 		
 		//Reading in the HW name
 		char hwName[MAXSIZE]; 
-		if(sscanf(args[2], "%[^\n]",hwName) != 1){
+		if(sscanf(args[2], "%23[^\n]",hwName) != 1){
 				fprintf(stderr,"Invalid hwName");
 				exit(-1);
 		}
@@ -99,7 +106,47 @@ void append(char** args){
 				exit(-1);
 		}
 
-		printf("%s %s %.2f %.2f\n", hwName,hwType, maxScore, actScore);
+
+		//ensuring it is a proper string by adding the terminating character to the end
+		hwName[NAME_SIZE - 1] = 0;
+
+		// declaring first instance of AssignmentRecord from Record.h
+		AssignmentRecord ar;
+		//By default valid should be 0 which in this case is true
+		ar.valid = 0;
+		//using strcpy() to assing hwName to ar.name
+		strcpy(ar.name, hwName); 
+		//Setting the read-in maxScore to our record
+		ar.max_score = maxScore;
+		// Setting the actual score to our record
+		ar.score = actScore;
+		
+
+		//Iterating through type_name which is an array in Record.h
+		for(int i = 0; i < SIZE_TYPE_NAMES ;++i ){
+				// iterating until we find the int position of the hwType
+				if(strcmp(type_names[i],hwType) == 0){
+						//assigning int position to ar.type
+						ar.type = i;
+						//since it should only match once breaking immediately
+						break; 
+				}
+		}
+
+		/*
+
+				TODO:
+				
+				fix offset since it is currently overwriting the previous record.
+				should probably use lseek()
+		   */
+				
+		//writing out our ar(record) to our file
+		if(write(fd, &ar, sizeof(AssignmentRecord)) != sizeof(AssignmentRecord)){
+				perror("Write");
+				exit(-1);
+		}
+		
 }
 
 
